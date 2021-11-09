970eb3096d5ea8a98b3226f0ed91939403784cca
Samuel Ventura <samuel.ventura@yeico.com>
2021-11-08 18:26:47 -0600

    v0.1.0 hex.pm

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule NervesBackdoor.MixProject do
   use Mix.Project
 
+  @github_organization "samuelventura"
+  @app :nerves_backdoor
+  @source_url "https://github.com/#{@github_organization}/#{@app}"
+  @version Path.join(__DIR__, "VERSION")
+           |> File.read!()
+           |> String.trim()
+
   def project do
     [
-      app: :nerves_backdoor,
+      app: @app,
+      version: @version,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      description: description()
     ]
   end
 
@@ -23,7 +34,32 @@ defmodule NervesBackdoor.MixProject do
     [
       {:plug, "~> 1.7"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "test", "mix.*", "*.exs", "*.md", ".gitignore", "LICENSE", "VERSION"],
+      maintainers: ["Samuel Ventura"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp description do
+    """
+    Nerves Backdoor - Setup and Maintenance RESTish API
+    """
   end
 end
