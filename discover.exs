59c63f20eb2f0b40cf65e697e4c20b2754c764d4
Samuel Ventura <samuel.ventura@yeico.com>
2021-12-02 16:55:03 -0600

    discovery loop with 1s timeout

--- discover.exs
+++ discover.exs
@@ -1,9 +1,17 @@
 #mix run discover.exs --no-start
 
+loop = fn (socket, loop) ->
+  case :gen_udp.recv(socket, 1024, 1000) do
+    {:ok, msg} ->
+      IO.inspect(msg)
+      loop.(socket, loop)
+    {:error, msg} ->
+      IO.inspect(msg)
+  end
+end
+
 cmd = Jason.encode! %{action: "id", name: "nerves"}
 {:ok, socket} = :gen_udp.open(0, active: false, broadcast: true, multicast_ttl: 4, multicast_loop: false)
 :ok = :gen_udp.send(socket, {255, 255, 255, 255}, 31680, cmd)
-msg = :gen_udp.recv(socket, 1024, 1000)
+loop.(socket, loop)
 :ok = :gen_udp.close(socket)
-
-IO.inspect(msg)
